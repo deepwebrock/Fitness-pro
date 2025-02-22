73e695bb37cc2a236279ef46968b86f118823a25
deepwebrock <sandeepsingh.1id@gmail.com>
2025-02-23 00:42:54 +0530

    Updated features and fixed bugs

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -62,7 +62,7 @@ const Hero = () => {
       {/* Background Image with overlay */}
       <div 
         className="absolute inset-0 bg-cover bg-center" 
-        style={{ backgroundImage: "url('background.jpg')" }}
+        style={{ backgroundImage: "url('background1.jpg')" }}
       >
         <div className="absolute inset-0 bg-black opacity-50 dark:opacity-60"></div>
       </div>
